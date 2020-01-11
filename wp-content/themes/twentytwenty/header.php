052ac136670bcac46ed338c18be8dc2417927f9d
vuthanhvien <vuthanhvien@github.io>
2020-01-11 09:28:25 +0700

    new

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -21,6 +21,7 @@
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<link rel="stylesheet" type="text/css" href="/wp-content/themes/twentytwenty/custom.css">
 		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i&display=swap&subset=vietnamese" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
 		<?php wp_head(); ?>
  
 	</head>
